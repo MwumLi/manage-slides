93ff1ab4901af70aa25d2893cd1342acd641457e
MwumLi <mwumli@hotmail.com>
2020-06-04 09:14:30 +0800

    feat: show help without sub command

--- bin/index.js
+++ bin/index.js
@@ -13,6 +13,12 @@ const commands = [
     description: '初始化一个 Slide Manager 项目'
   },
   {
+    name: 'add',
+    command: 'add <slide name>',
+    
+    description: '新增一个 slide'
+  },
+  {
     name: 'serve',
     description: '本地运行'
   },
@@ -30,3 +36,11 @@ for (let { name, command, description } of commands) {
 }
 
 program.parse(process.argv);
+
+// https://github.com/tj/commander.js/issues/7#issuecomment-32448653
+// Check the program.args obj
+const NO_COMMAND_SPECIFIED = program.args.length === 0;
+
+if (NO_COMMAND_SPECIFIED) {
+  program.help();
+}
